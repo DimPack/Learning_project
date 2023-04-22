b56666bb88327899b171d6be0afcd60a513f91e6
Dmytro <csourgg@gmail.com>
2023-04-22 23:09:07 +0300

    I am trying to llop the question in which there should be only numbers

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-let numberOfFilms =  prompt('Скільки фільмів ви вже подивилися?');
+"use strict"
+
+let numberOfFilms = prompt('Скільки фільмів ви вже подивилися?');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let lastMovie = prompt('Який ваш один з останіх перегляних фільмів?');
+let ratingMovie = prompt('На скільки оцінете його?');
