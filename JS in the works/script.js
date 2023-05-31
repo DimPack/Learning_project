bc0cf79890557a5957aa8ce84ee160fa9482b5f3
Dima <csourgg@gmail.com>
2023-05-31 15:33:15 +0300

    work with elements html + JS (document, style,  classList.add, remove, append, prepend, before, after, replaceWith, innerHTML, insertAdjacentHTML)

--- JS in the works/script.js
+++ JS in the works/script.js
@@ -3,9 +3,10 @@
 const box = document.getElementById('box'),
       btns = document.getElementsByTagName('button'),
       circle = document.getElementsByClassName('circle'),
-      heart = document.querySelectorAll('.heart'),
-      oneHeart = document.querySelector('.heart');
-
+      wrapper = document.querySelector('.wrapper'), // він є родителем і в середині нього буде шукати
+      heart = wrapper.querySelectorAll('.heart'),
+      oneHeart = wrapper.querySelector('.heart');
+      
 console.dir(box);
 
 // box.style.backgroundColor = 'pink';
@@ -22,5 +23,27 @@ btns[2].style.borderRadius = '50%';
 //     heart[i].style.cssText = 'background-color: brown; width: 200px; height: 200px' 
 // }
 heart.forEach(item => {
-    item.style.cssText = 'background-color: brown; width: 100px; height: 100px' 
+    item.style.backgroundColor =  'pink'
+    // item.style.cssText = 'background-color: brown; width: 100px; height: 100px' 
 });
+
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут був я!'); //устарівший метод
+
+div.classList.add('black');
+
+document.body.append(div); //в кінець body добавляємо елемент
+
+// wrapper.append(div); // wrapper він 1 елемент тому просто знаходим гово і добаляєм його вкінець
+// wrapper.prepend(div); // wrapper в початок данного родителя
+// heart[1].before(div); // спереді певного елементу
+// heart[1].after(div); // сзаді певного елементу
+
+// circle[0].remove();
+
+heart[0].replaceWith(circle[0]); // заміняєм один елемент другим спочатку який елемент потім на який 
+
+div.innerHTML = "<p>Hello World</p>"; // html сойства добив
+// div.textContent = "hello"; // html тільки текст без html структури
+
+div.insertAdjacentHTML('afterend',' <h2>Hey!</h2>'); // 1 аршумент вказує позицію елементу що вставляємо
